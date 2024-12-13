53a2783a3b5d86d9da48f32913a5116cc4b9271f
허윤 <>
2024-12-13 14:39:40 +0900

    TRS insert values

---
+++ TRS_table.sql
@@ -0,0 +1,12 @@
+insert into Typhoon_Rating_system
+values(trs_rno_seq.nextval,18, 33, '열대저기압');
+insert into Typhoon_Rating_system
+values(trs_rno_seq.nextval,34, 47, '약');
+insert into Typhoon_Rating_system
+values(trs_rno_seq.nextval,48, 63, '중');
+insert into Typhoon_Rating_system
+values(trs_rno_seq.nextval,64, 84, '강');
+insert into Typhoon_Rating_system
+values(trs_rno_seq.nextval,85, 104, '매우 강');
+insert into Typhoon_Rating_system
+values(trs_rno_seq.nextval,105,999, '초강력');

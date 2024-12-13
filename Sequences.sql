e9b76224f2ee5bdc2bd8fdb8323761dfabdb587c
허윤 <>
2024-12-13 14:36:43 +0900

    시퀀스 생성

---
+++ Sequences.sql
@@ -0,0 +1,15 @@
+create sequence impact_impno_seq
+                start with 1
+                INCREMENT by 1;
+
+create sequence region_regno_seq
+                start with 1
+                INCREMENT by 1;
+                
+create sequence weather_wno_seq
+                start with 1
+                INCREMENT by 1;
+                
+create sequence trs_rno_seq
+                start with 10
+                INCREMENT by 10;

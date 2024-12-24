53a3d24b71a2e93e5035e2a823f07a5dd503f301
허윤 <>
2024-12-24 14:16:15 +0900

    Weather_insert

--- WeatherData.sql
+++ WeatherData.sql
@@ -1,1 +1,120 @@
 --Weather Å×ÀÌºí¿¡ µ¥ÀÌÅÍ Ãß°¡ÇÏ±â
+-- ÅÂÇ³ '¸µ¸µ' (2001-09-01 ~ 2001-09-05)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 120, 150, 300, 970, 35.5, 128.2, 'Start',1);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 150, 160, 350, 960, 35.8, 128.4, 'Middle',1);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 180, 170, 400, 950, 36.0, 128.6, 'End',1);
+
+-- ÅÂÇ³ 'Æã¼Ï' (2002-06-16 ~ 2002-06-23)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 110, 130, 250, 980, 34.2, 126.5, 'Start',2);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 140, 140, 300, 970, 34.5, 126.7, 'Middle',2);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 150, 350, 960, 34.8, 126.9, 'End',2);
+
+-- ÅÂÇ³ '¸Å¹Ì' (2003-09-06 ~ 2003-09-11)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 130, 140, 280, 975, 33.7, 130.4, 'Start',3);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 160, 330, 965, 33.9, 130.6, 'Middle',3);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 180, 170, 380, 955, 34.1, 130.8, 'End',3);
+
+-- ÅÂÇ³ '´ÏÆÄÅ¹' (2004-07-03 ~ 2004-07-06)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 100, 120, 230, 985, 33.5, 134.0, 'Start',4);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 130, 130, 270, 975, 33.7, 134.2, 'Middle',4);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 150, 140, 320, 965, 33.9, 134.4, 'End',4);
+
+-- ÅÂÇ³ '¸À»ç' (2005-07-28 ~ 2005-08-02)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 110, 130, 240, 980, 32.7, 131.2, 'Start',5);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 140, 140, 290, 970, 32.9, 131.4, 'Middle',5);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 150, 340, 960, 33.1, 131.6, 'End',5);
+
+-- ÅÂÇ³ '¿ìÄô' (2006-08-13 ~ 2006-08-19)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 100, 120, 200, 985, 33.0, 134.8, 'Start',6);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 130, 130, 250, 975, 33.2, 135.0, 'Middle',6);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 150, 140, 300, 965, 33.4, 135.2, 'End',6);
+
+-- ÅÂÇ³ 'ÇÇÅä' (2007-07-17 ~ 2007-07-21)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 130, 150, 270, 980, 34.0, 126.3, 'Start',7);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 160, 320, 970, 34.2, 126.5, 'Middle',7);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 180, 170, 370, 960, 34.4, 126.7, 'End',7);
+
+-- ÅÂÇ³ 'ÂùÈ¨' (2008-07-10 ~ 2008-07-14)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 120, 140, 260, 975, 33.8, 128.9, 'Start',8);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 150, 150, 310, 965, 34.0, 129.1, 'Middle',8);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 170, 160, 360, 955, 34.2, 129.3, 'End',8);
+
+-- ÅÂÇ³ '¹Ì¸®³»' (2009-10-02 ~ 2009-10-08)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 110, 130, 220, 980, 35.0, 128.0, 'Start',9);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 140, 140, 270, 970, 35.2, 128.2, 'Middle',9);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 150, 320, 960, 35.4, 128.4, 'End',9);
+
+-- ÅÂÇ³ '¸Þ±â' (2010-10-18 ~ 2010-10-24)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 140, 160, 290, 975, 36.0, 129.5, 'Start',10);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 170, 170, 340, 965, 36.2, 129.7, 'Middle',10);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 200, 180, 400, 955, 36.4, 129.9, 'End',10);
+
+-- ÅÂÇ³ '¹«ÀÌÆÄ' (2011-07-01 ~ 2011-07-07)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 120, 140, 260, 980, 34.3, 130.4, 'Start',11);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 150, 150, 310, 970, 34.5, 130.6, 'Middle',11);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 180, 160, 360, 960, 34.7, 130.8, 'End',11);
+-- ÅÂÇ³ '´ã·¹ÀÌ' (2012-11-26 ~ 2012-11-30)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 120, 140, 250, 980, 33.2, 128.0, 'Start',12);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 150, 150, 300, 970, 33.4, 128.2, 'Middle',12);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 170, 160, 350, 960, 33.6, 128.4, 'End',12);
+
+-- ÅÂÇ³ 'ÇÇÅä' (2013-09-18 ~ 2013-09-23)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 130, 150, 270, 980, 34.0, 126.7, 'Start',13);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 160, 320, 970, 34.2, 126.9, 'Middle',13);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 180, 170, 370, 960, 34.4, 127.1, 'End',13);
+
+-- ÅÂÇ³ '¸¶Æ®¸ð' (2014-07-17 ~ 2014-07-22)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 140, 160, 290, 975, 33.9, 129.5, 'Start',14);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 170, 170, 340, 965, 34.1, 129.7, 'Middle',14);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 200, 180, 400, 955, 34.3, 129.9, 'End',14);
+
+-- ÅÂÇ³ '³ëÀ»' (2015-10-13 ~ 2015-10-20)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 120, 140, 260, 980, 35.2, 128.0, 'Start',15);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 150, 150, 310, 970, 35.4, 128.2, 'Middle',15);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 170, 160, 360, 960, 35.6, 128.4, 'End',15);
+
+-- ÅÂÇ³ 'Â÷¹Ù' (2016-07-02 ~ 2016-07-10)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 130, 150, 280, 980, 34.7, 129.0, 'Start',16);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 160, 330, 970, 34.9, 129.2, 'Middle',16);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 180, 170, 380, 960, 35.1, 129.4, 'End',16);
+
+-- ÅÂÇ³ 'µ¶¼ö¸®' (2017-08-14 ~ 2017-08-21)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 140, 160, 290, 975, 35.0, 130.0, 'Start',17);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 170, 170, 340, 965, 35.2, 130.2, 'Middle',17);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 200, 180, 390, 955, 35.4, 130.4, 'End',17);
+
+-- ÅÂÇ³ '»Ú¶ó»ß·é' (2018-07-11 ~ 2018-07-16)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 110, 130, 240, 980, 33.8, 127.5, 'Start',18);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 140, 140, 290, 970, 34.0, 127.7, 'Middle',18);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 150, 340, 960, 34.2, 127.9, 'End',18);
+
+-- ÅÂÇ³ '´Ù³ª½º' (2019-07-14 ~ 2019-07-19)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 120, 140, 260, 980, 33.9, 127.2, 'Start',19);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 150, 150, 310, 970, 34.1, 127.4, 'Middle',19);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 170, 160, 360, 960, 34.3, 127.6, 'End',19);
+
+-- ÅÂÇ³ '³¶Ä«' (2020-10-10 ~ 2020-10-16)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 130, 150, 280, 980, 34.6, 128.8, 'Start',20);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 160, 330, 970, 34.8, 129.0, 'Middle',20);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 180, 170, 380, 960, 35.0, 129.2, 'End',20);
+
+-- ÅÂÇ³ '·çÇÍ' (2021-09-06 ~ 2021-09-09)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 110, 130, 230, 985, 35.3, 128.5, 'Start',21);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 140, 140, 280, 975, 35.5, 128.7, 'Middle',21);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 150, 330, 965, 35.7, 128.9, 'End',21);
+
+-- ÅÂÇ³ 'Â÷¹Ù' (2022-07-02 ~ 2022-07-08)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 120, 140, 240, 980, 34.4, 129.6, 'Start',22);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 150, 150, 290, 970, 34.6, 129.8, 'Middle',22);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 170, 160, 340, 960, 34.8, 130.0, 'End',22);
+
+-- ÅÂÇ³ '¶õ' (2023-08-01 ~ 2023-08-05)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 130, 150, 270, 980, 33.7, 128.4, 'Start',23);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 160, 160, 320, 970, 33.9, 128.6, 'Middle',23);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 180, 170, 370, 960, 34.1, 128.8, 'End',23);
+
+-- ÅÂÇ³ 'Ç®¶ó»ê' (2024-09-15 ~ 2024-09-20)
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 120, 140, 250, 980, 33.6, 129.1, 'Start',24);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 150, 150, 300, 970, 33.8, 129.3, 'Middle',24);
+INSERT INTO Weather VALUES (weather_wno_seq.NEXTVAL, 170, 160, 350, 960, 34.0, 129.5, 'End',24);
+
